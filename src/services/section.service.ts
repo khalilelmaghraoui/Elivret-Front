70e74d42f9e208cab950783ca32c7176d7546136
e22026813 <khalil.EL-MAGHRAOUI@etu.univ-amu.fr>
2022-12-30 22:31:15 +0100

    test

--- src/services/section.service.ts
+++ src/services/section.service.ts
@@ -1,22 +1,28 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SectionService {
+export class SectionService implements OnInit{
 
   readonly BaseUrl="http://localhost:8080/api/elivret/"
   readonly Get="http://localhost:8080/api/elivret/1/sections"
 
-
+  sid:any;
   constructor(private http:HttpClient) {
 
   
    }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
    
 
-   public section(){
-    return this.http.get("http://localhost:8080/api/elivret/1/sections");
+   public section(sid:any){
+    return this.http.get(`http://localhost:8080/api/elivret/${{sid}}/sections`);
    }
+
+  
+
 }
