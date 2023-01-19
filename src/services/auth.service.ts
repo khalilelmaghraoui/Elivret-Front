c68ae4afa05473b9199ad03c9caf95cdd6c956ba
Your Name <you@example.com>
2023-01-19 13:26:07 +0100

    added login

---
+++ src/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  BaseUrl = "http://localhost:8080/secu-users";
+
+
+  constructor(private http:HttpClient) { }
+
+  public login(loginDetails:any){
+    return this.http.post("http://localhost:8080/secu-users/login", loginDetails, {responseType: 'text'});
+  }
+
+
+}
