7f38d5f21a79a143a8e335600f8442187f89d4cf
Your Name <you@example.com>
2023-01-19 18:57:11 +0100

    added login

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/services/auth.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginDetails= {
+    username:'',
+    password: ''
+  }
+
+  constructor(private authService:AuthService,private route:ActivatedRoute){
+  }
+
+  loginForm(){
+    if(this.loginDetails.username && this.loginDetails.password){
+      this.authService.login(this.loginDetails).subscribe((data)=>{
+      })
+    }
+    
+  }
+
+
+
+
 
 }
