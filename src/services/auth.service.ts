b65d4ae5e635ccdc46239c6e745237e3fbeebe8e
Your Name <you@example.com>
2023-02-14 12:36:06 +0100

    g

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -19,6 +19,7 @@ export class AuthService {
   public logout(){
     localStorage.removeItem("token");
     localStorage.removeItem("role");
+    localStorage.removeItem("personId");
     this.router.navigate(['login']);
   }
 
